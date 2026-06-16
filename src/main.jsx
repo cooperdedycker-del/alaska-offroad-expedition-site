3d839fae1aaac4575e0d90cff23810052311ed68
cooperdedycker-del <cooperdedycker@gmail.com>
2026-06-16 12:43:42 -0800

    Add reservation confirmation page and participant details

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AlaskaOffroadExpedition from "./AlaskaOffroadExpedition.jsx"; // your current main page component
-import LionsClubPage from "./pages/LionsClub.jsx"; // we'll create this next
-import "./index.css"; // whatever you already import for Tailwind
+import AlaskaOffroadExpedition from "./AlaskaOffroadExpedition.jsx";
 import ReservationConfirmed from "./pages/ReservationConfirmed.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-  <Route path="/" element={<AlaskaOffroadExpedition />} />
-  <Route path="/lionsclub" element={<LionsClubPage />} />
-  <Route
-    path="/reservation-confirmed"
-    element={<ReservationConfirmed />}
-  />
-</Routes>
+        <Route path="/" element={<AlaskaOffroadExpedition />} />
+        <Route
+          path="/reservation-confirmed"
+          element={<ReservationConfirmed />}
+        />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
